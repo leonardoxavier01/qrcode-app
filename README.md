9c16b27062498d50d4b4c7e481b7e9e2275d9f18
Leonardo -Software Developer <leonardoxavier092@gmail.com>
2023-01-03 00:15:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# QrCode App in React
---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+  colors: {
+    primary: '#05A8AA',
+    secondary: '#E3E8F3',
+    background: '#ffffff',
+    text: '#0E161D',
+    white: '#ffffff'
+  },
+
+  box: {
+    borderColor: '#E3E8F3',
+    borderRadius: '2rem',
+    boxShadow: '-1px 4px 15px -5px rgba(200,201,203,0.95)'
+  }
+};
